52a9498d36129ed81c07ed06c77b34cf5f94681f
Francesco Bruschi <fbrusch@gmail.com>
2022-06-20 10:48:31 +0000

    add bouncing

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import time
 
 pygame.init()
 
+x = 0
 screen = pygame.display.set_mode((300,300))
 
-pygame.draw.rect(screen, (100,100,100),   (0,0,300,300))
+vx = 3
+while True:
+  pygame.draw.rect(screen, (100,100,100),   (0,0,300,300))
+  
+  pygame.draw.rect(screen, (0,255,0),   (x,0,50,50))
+  
+  pygame.display.flip() # draw everything
+  time.sleep(0.02)
+  
+  x = x + vx
 
-pygame.draw.rect(screen, (0,255,0),   (0,0,50,50))
+  # se l'oggetto sta uscendo dal mondo: esegui vx = -vx
+  if x + 50 > 300:
+    vx = -vx
 
-pygame.display.flip() # draw everything
-time.sleep(1)
-
-pygame.draw.rect(screen, (100,100,100),   (0,0,300,300))
-
-pygame.draw.rect(screen, (0,255,0),   (10,0,50,50))
-
-pygame.display.flip()
+  if x < 0:
+    vx = -vx
+    
