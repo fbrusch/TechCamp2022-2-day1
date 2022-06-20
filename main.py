f48decbaefb29ddb47170b3eaecfb1c2172b1a4f
Francesco Bruschi <fbrusch@gmail.com>
2022-06-20 10:21:08 +0000

    move the rectangle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 
@@ -6,4 +7,13 @@ screen = pygame.display.set_mode((300,300))
 
 pygame.draw.rect(screen, (100,100,100),   (0,0,300,300))
 
+pygame.draw.rect(screen, (0,255,0),   (0,0,50,50))
+
 pygame.display.flip() # draw everything
+time.sleep(1)
+
+pygame.draw.rect(screen, (100,100,100),   (0,0,300,300))
+
+pygame.draw.rect(screen, (0,255,0),   (10,0,50,50))
+
+pygame.display.flip()
